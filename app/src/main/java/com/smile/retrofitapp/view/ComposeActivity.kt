a74d4d56e6745b62bb4678accf6851c4380e515b
SmileChaoLee <leechao1959@gmail.com>
2025-05-23 03:42:11 -0400

    Refined the UI

--- app/src/main/java/com/smile/retrofitapp/view/ComposeActivity.kt
+++ app/src/main/java/com/smile/retrofitapp/view/ComposeActivity.kt
@@ -6,23 +6,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.smile.retrofitapp.models.Language
 import com.smile.retrofitapp.view.ui.theme.RetrofitAppTheme
 import com.smile.retrofitapp.viewmodels.LanguagesVewModel
@@ -33,6 +37,8 @@ class ComposeActivity : ComponentActivity() {
 
     private val viewModel: LanguagesVewModel by viewModels()
     private var counter = 0
+
+    private var defaultFontSize = 20.sp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,7 +58,7 @@ class ComposeActivity : ComponentActivity() {
             LaunchedEffect(Unit) {
                 viewModel.loadData()
             }
-            CreateView(viewModel.languages.value)
+            DisplayLanguages(viewModel.languages.value)
         }
     }
 
@@ -67,66 +73,66 @@ class ComposeActivity : ComponentActivity() {
     }
 
     @Composable
-    fun CreateView( languages: List<Language>) {
-        Log.d(TAG, "CreateView")
-        val columnVertical = 30.dp
-        val buttonWidth = 200.dp
-        val buttonHeight = 60.dp
-        val columnModifier = Modifier.fillMaxSize().padding(vertical = columnVertical)
-        Column(modifier = columnModifier,
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = {
-                if (counter%2 == 0) {
-                    viewModel.setLanguages(getAllLanguages(1))
-                } else {
-                    viewModel.loadData()
-                }
-                counter++
-                Log.d(TAG, "CreateView.Button.Clicked.counter = $counter")
-            }, modifier = Modifier.size(width = buttonWidth, height = buttonHeight))
-            { Text(text = "Update languages") }
-        }
-        val listVertical = columnVertical + buttonHeight + 30.dp
-        val listModifier = Modifier.fillMaxSize().padding(vertical = listVertical)
-        if (languages.isNotEmpty()) {
-            Log.d(TAG, "CreateView.DisplayLanguages")
-            DisplayLanguages(languages, listModifier)
-        }
-    }
-    @Composable
-    fun DisplayLanguages(languages: List<Language>, modifier: Modifier) {
+    fun DisplayLanguages(languages: List<Language>) {
         Log.d(TAG, "DisplayLanguages.languages.size = ${languages.size}")
-        LazyColumn(modifier = modifier
-            .background(color = androidx.compose.ui.graphics.Color.LightGray)) {
-            items(languages) { language ->
-                val textModifier = Modifier.padding(start = 2.dp, end = 0.dp)
-                val textHeight = 25.dp
-                Row (modifier = Modifier.fillMaxSize()) {
-                    language.id?.toString()?.let {
-                        Text(text = it.padStart(3, ' '),
-                            modifier = textModifier.size(width = 40.dp, height = textHeight),
-                            fontFamily = FontFamily.Monospace,
-                            color = androidx.compose.ui.graphics.Color.Red)
+        val textFontSize = (defaultFontSize.value * 1f).sp
+        Column(modifier = Modifier.fillMaxSize()
+            .background(color = Color(0xff90e5c4))) {
+            Text(text = "Languages List", fontSize = textFontSize, color = Color.Blue)
+            HorizontalDivider(color = Color.Black,
+                modifier = Modifier.fillMaxWidth().size(10.dp))
+            Log.d(TAG, "DisplayLanguages.LazyColumn")
+            LazyColumn(modifier = Modifier.weight(1f)) {
+                items(languages) { language->
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        language.id?.toString()?.let {
+                            Text(text = it.padStart(3, ' '),
+                                modifier = Modifier.weight(2f),
+                                fontFamily = FontFamily.Monospace, fontSize = textFontSize,
+                                color = Color.Red)
+                        }
+                        Text(
+                            text = language.langNo ?: "",
+                            modifier = Modifier.weight(1f),
+                            fontFamily = FontFamily.Monospace, fontSize = textFontSize,
+                            color = Color.Red
+                        )
+                        Text(
+                            text = language.langNa ?: "",
+                            Modifier.weight(4f),
+                            fontFamily = FontFamily.Monospace, fontSize = textFontSize,
+                            color = Color.Red
+                        )
+                        Text(
+                            text = language.langEn ?: "",
+                            Modifier.weight(6f),
+                            fontFamily = FontFamily.Monospace,fontSize = textFontSize,
+                            color = Color.Red
+                        )
                     }
-                    Text(
-                        text = language.langNo ?: "",
-                        modifier = textModifier.size(width = 40.dp, height = textHeight),
-                        fontFamily = FontFamily.Monospace,
-                        color = androidx.compose.ui.graphics.Color.Red
-                    )
-                    Text(
-                        text = language.langNa ?: "",
-                        modifier = textModifier.size(width = 100.dp, height = textHeight),
-                        fontFamily = FontFamily.Monospace,
-                        color = androidx.compose.ui.graphics.Color.Red
-                    )
-                    Text(
-                        text = language.langEn ?: "",
-                        modifier = textModifier.size(width = 200.dp, height = textHeight),
-                        fontFamily = FontFamily.Monospace,
-                        color = androidx.compose.ui.graphics.Color.Red
-                    )
+                    HorizontalDivider(color = Color.Blue,
+                        modifier = Modifier.fillMaxWidth().size(5.dp))
                 }
+            }
+            Log.d(TAG, "DisplayLanguages.Column.Button")
+            Column(modifier = Modifier.fillMaxWidth()
+                .background(color = Color.Blue),
+                horizontalAlignment = Alignment.CenterHorizontally
+                , verticalArrangement = Arrangement.Center) {
+                Button(onClick = {
+                    if (counter % 2 == 0) {
+                        viewModel.setLanguages(getAllLanguages(1))
+                    } else {
+                        viewModel.loadData()
+                    }
+                    counter++
+                    Log.d(TAG, "DisplayLanguages.Button.Clicked.counter = $counter")
+                }, colors = ButtonColors(containerColor = Color.Cyan,
+                    disabledContainerColor = Color.DarkGray,
+                    contentColor = Color.Red,
+                    disabledContentColor = Color.LightGray)
+                ) { Text(text = "Update languages",
+                    fontSize = textFontSize) }
             }
         }
     }
